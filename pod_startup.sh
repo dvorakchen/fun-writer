7b0aa7cc06ccd6c6fed6825a5c2d555b1095e22a
DvorakChen <dvorakchen@outlook.com>
2025-09-03 23:17:00 +0800

    fix: update pod startup script to create specific data directory and adjust volume path

--- pod_startup.sh
+++ pod_startup.sh
@@ -1,8 +1,8 @@
-mkdir ~/data -p
+mkdir ~/data/fun-writer -p
 
 podman build . -t fun-writer-web
 
 podman pod create --name fun-writer-pod -p 3000:3000
 # podman run --pod fun-writer-pod --name postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=123123 -e POSTGRES_DB=fun-writer -d hub.aiursoft.cn/postgres:latest
-podman run --pod fun-writer-pod --name postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=123123 -e POSTGRES_DB=fun-writer -v ~/data:/var/lib/postgresql/data -d hub.aiursoft.cn/postgres:latest   
+podman run --pod fun-writer-pod --name postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=123123 -e POSTGRES_DB=fun-writer -v ~/data/fun-writer:/var/lib/postgresql/data -d hub.aiursoft.cn/postgres:latest   
 podman run --pod fun-writer-pod -d fun-writer-web
